01e106c190c59e0e1e2482a8c6e7f25bee70f744
jejejeje2912 <tonytranb2b@gmail.com>
2021-05-11 14:24:59 +0200

    Update color #FF3E7B

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -5,7 +5,7 @@ export const baseColors = {
   primary: "#aa8929",
   primaryBright: "#aa8929",
   primaryDark: "#aa8929",
-  secondary: "#7645D9",
+  secondary: "#FF3E7B",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -23,7 +23,7 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#452A7A",
+  text: "#FF3E7B",
   textDisabled: "#BDC2C4",
   textSubtle: "#FF3E7B",
   borderColor: "#E9EAEB",
