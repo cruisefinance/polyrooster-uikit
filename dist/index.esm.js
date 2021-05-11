ab3e7804cc83ed63b829ec225b5dc42eb8f58442
jejejeje2912 <tonytranb2b@gmail.com>
2021-05-11 14:46:03 +0200

    Update color da583e

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2855,7 +2855,7 @@ var baseColors = {
     primary: "#aa8929",
     primaryBright: "#aa8929",
     primaryDark: "#aa8929",
-    secondary: "#7645D9",
+    secondary: "#da583e",
     success: "#31D0AA",
     warning: "#FFB237",
 };
